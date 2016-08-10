597e3f87e2b1279128e5ca0857113a6ecbd417d8
Alexis Andrieu <andrieu.alexis@hotmail.com>
2016-08-10 06:56:48 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -19,8 +19,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
-            return fetch(event.request);
-            // return response || fetch(event.request);
+            return response || fetch(event.request);
         })
     );
 });
