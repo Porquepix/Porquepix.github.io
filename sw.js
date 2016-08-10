4712ff4f16b8f7a6dee06cafc419af371eda38f8
Alexis Andrieu <andrieu.alexis@hotmail.com>
2016-08-10 07:33:41 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,15 @@
-var CACHE_NAME = 'alexis-andrieu';
-var URL_TO_CACHE = [
-    '/',
-    'index.html',
-    'app.css',
-    'app.js',
-    'logo.png'
-];
-
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function(cache) {
-            return cache.addAll(URL_TO_CACHE);
-        })
-    );
+  event.waitUntil(
+    caches.open('alexis-andrieu').then(function(cache) {
+      return cache.addAll([
+        '/',
+        'index.html',
+        'app.css',
+        'app.js',
+        'logo.png'
+      ]);
+    })
+  );
 });
 
 
